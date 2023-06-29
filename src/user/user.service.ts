46b43504888f02888aae1e3f029ceb9dfecd1835
Robert Santos <robertdosantos09@gmail.com>
2023-06-29 02:17:21 -0300

    feat: Adicionando o modulo de user.

---
+++ src/user/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/db/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: { name: createUserDto.name, email: createUserDto.email },
+    });
+  }
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+}
